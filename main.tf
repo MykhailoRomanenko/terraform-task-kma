0780202b1b2a0f31af4ea79f14bb61429dd4f343
MykhailoRomanenko <moromanenko@gmail.com>
2024-04-13 10:22:41 +0300

    define ec2

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.16"
+    }
+  }
+
+  required_version = ">=1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "lab5_ec2" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Lab5Romanenko"
+  }
+}
